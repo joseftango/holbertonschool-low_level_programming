ab781195e6dcc7d8e00e11f63840a2c46dbc2962
joseftango <4279@holbertonschool.com>
2022-02-13 02:45:17 -0800

    9time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * add - writes the character c to stdout
- *@a: input
- *@b: input
+ * times_table - writes the character c to stdout
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int add(int a, int b)
-{ int sum;
-	sum = a + b;
-	return (sum);
+void times_table(void)
+{int m, i, j;
+		for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			m = i * j;
+		if (m != 0)
+		{
+			if (m > 0 && m <= 9)
+				{
+				printf(",  ");
+				}
+				else if (j <= 9)
+				{
+				printf(", ");
+				}
+		}
+		else if (j > 0)
+		{
+			printf(",  ");
+		}
+				printf("%d", m);
+		}
+		printf("\n");
+	}
 }
