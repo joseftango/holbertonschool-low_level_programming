bb634c6f00e2138704d1b3ddea0d89fe86c2674d
joseftango <4279@holbertonschool.com>
2022-02-13 01:56:54 -0800

    hi

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - writes the alphabets 10 times in a row
+ * Return: On success void.
+ */
+
+void print_alphabet_x10(void)
+{
+  char c;
+  int a;
+
+  for (a = 0; a <= 9; a++)
+    {
+      for (c = 'a'; c <= 'z'; c++)
+	{
+	  _putchar(c);
+	}
+      _putchar('\n');
+    }
+
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+S~#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - writes the alphabets 10 times in a row
+ * Return: On success void.
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	}
+
+}
+
