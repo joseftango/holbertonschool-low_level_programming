1126ce77c6db6d077acba2bdb00c3158f4d8cd8a
joseftango <4279@holbertonschool.com>
2022-02-14 11:36:39 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int a;
+int b;
+
+    a = 98;
+    b = 42;
+
+    return (0);
+}
