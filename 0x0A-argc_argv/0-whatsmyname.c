86685ddc2281eca63fb50d7f5efa4fda7f5f099c
joseftango <4279@holbertonschool.com>
2022-02-28 03:31:54 -0800

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *@argc: argument count
+ *@argv: argument value
+ * Return: Always 0.
+ */
+
+
+int main(int argc,char *argv[])
+{
+int x;
+	if(x=0;x<argc;x++)
+	printf("%s",argv[x]);
+}
+
+
