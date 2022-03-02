7c948773a495614eba2bf2103e69e75b03d21c29
joseftango <4279@holbertonschool.com>
2022-03-02 11:35:01 -0800

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *print_rev- prints  a string
+ *rev_string- prints  a string
  * @s: input
  * Return: void
  */
 void print_rev(char *s)
 {
-	int i, b;
-	int a = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		a = a + 1;
-	}
-	for (b = a - 1; b >= 0 ; b--)
-	{
-		_putchar(s[b]);
-	}
-	_putchar('\n');
+int a = 0, length;
+while (*s != '\0')
+{
+	s++;
+	a++;
+}
+for (length = a; length >= 0; length--)
+{
+	s--;
+	putchar(*s);
+}
+	putchar('\n');
 }
