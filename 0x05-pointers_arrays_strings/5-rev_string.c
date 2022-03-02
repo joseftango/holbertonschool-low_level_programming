8be5e892a115b5eaa7b3d949e2c4c233c9b6c22e
joseftango <4279@holbertonschool.com>
2022-03-02 11:32:33 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,19 +5,18 @@
  * @s: input
  * Return: void
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int i, b, t;
-	int a = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		a = a + 1;
-	}
-	for (b = 0; b < a / 2; b++)
-	{
-		t = s[b];
-		s[b] = s[a - b - 1];
-		s[a - b - 1] = t;
-	}
+int a = 0, length;
+while (*s != '\0')
+{
+	s++;
+	a++;
+}
+for (length = a; length >= 0; length--)
+{
+	s--;
+	putchar(*s);
+}
+	putchar('\n');
 }
