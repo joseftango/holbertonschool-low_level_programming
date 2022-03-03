2de1837e78902f2afb54da425b1eeee4f8ad6ba2
joseftango <4279@holbertonschool.com>
2022-03-03 12:42:30 -0800

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  *print_array- prints  a string
  * @a: input
@@ -8,14 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-int i;
+	int i;
 
-for (i = 0; i < n; i++)
-{
-	printf("%d, ", a[i]);
-
-if (i != n - 1)
-	printf(", ");
-}
-	_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
 }
