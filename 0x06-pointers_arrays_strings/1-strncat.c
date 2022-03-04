86d3e30bed905e123553fd3debc13d60f04df895
joseftango <4279@holbertonschool.com>
2022-03-04 09:27:09 -0800

    upto

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,23 @@
 #include <unistd.h>
 #include "main.h"
-#include <string.h>
+
 /**
- **_strncat - glue the number of character  in the first input
- *@dest:pointer
+ **_strncpy -src overwrite in dest
+ *@dest:string pointer
  *@src:pointer
- *@n: number of character of src
- *@n:number of array
+ *@n: nummber of chatracter
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
-for (i = 0; dest[i] != '\0'; i++)
-{
-}
-for (j = 0; j < n && src[j] != '\0'; j++)
-{
-	dest[i + j] += src[j];
-}
-	dest[i + j] += '\0';
+	int c;
+
+	for (c = 0 ; c < n && src[c] != '\0' ; c++)
+	{
+		dest[c] = src[c];
+	}
+	dest[c] = '\0';
+
 	return (dest);
 }
