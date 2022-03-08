7b2caed7cbea88a267b8b799c443c613e7ae1220
joseftango <4279@holbertonschool.com>
2022-03-08 14:31:18 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,19 +6,17 @@
  *@argv : pointer of arrays
  * Return: Always 1.
  */
-int main(int argc, char *argv[])
+int main(int argc, char  *argv[])
 {
 int x;
 if (argc == 3)
 {
-int a = atoi(argv[1]);
-int b = atoi(argv[2]);
-x = a * b;
-printf ("%d\n", x);
+x = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", x);
 }
 else
 {
-printf ("Error\n");
+printf("Error\n");
 return (1);
 }
 return (0);
