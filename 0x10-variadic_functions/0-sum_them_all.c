bd171a22b823bd30efacd14e9d7b33fcd25e750c
joseftango <4279@holbertonschool.com>
2022-03-10 07:35:45 -0800

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 va_list total;
 int sum = 0;
-
+unsigned int x;
 if (n == 0)
 {
 return (0);
 }
 va_start(total, n);
-for (unsigned int x = 0; x < n; x++)
+for (x = 0; x < n; x++)
 {
 sum = sum + va_arg(total, int);
 }
