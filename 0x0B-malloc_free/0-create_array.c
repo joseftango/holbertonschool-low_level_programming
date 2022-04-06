535d56516d4ec7bf30f38be211ad6b536d99a88a
joseftango <4279@holbertonschool.com>
2022-04-06 15:39:09 +0100

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,16 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *ptr;
-    
+
 if (size == 0)
-return NULL;
+return (NULL);
 
 ptr = malloc(size * sizeof(char));
-
+if (ptr == NULL)
+return (NULL);
 for (i = 0; i < size; i++)
 {
-   ptr[i] = c;
+ptr[i] = c;
 }
-    return ptr;
+return (ptr);
 }
