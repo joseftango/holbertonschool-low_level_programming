5b56a34c1eed94049e360d46b53ce0830a9355fc
joseftango <4279@holbertonschool.com>
2022-04-10 03:56:08 +0100

    array rang

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,13 +7,16 @@
 */
 int *array_range(int min, int max)
 {
-int *p, i, tsize = (max - min) + 1;
+int *p, i, j = 0, tsize = (max - min) + 1;
 if (min > max)
 return (NULL);
 p = (int *) malloc(tsize * sizeof(int));
 if (p == NULL)
 return (NULL);
-for (i = 0; i < tsize ; i++)
-p[i] = i;
+for (i = min; i <= max ; i++)
+{
+p[j] = i;
+j++;
+}
 return (p);
 }
