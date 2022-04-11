cfa96e575916cded6fc35133d33c908ce7da7a9f
joseftango <4279@holbertonschool.com>
2022-04-11 22:12:08 +0100

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,17 @@
 void print_dog(struct dog *d)
 {
 if (d == NULL)
-return;
+printf("nothing");
 
 if (d->name == NULL)
-d->name = " (nil)";
-if (d->owner == NULL)
-d->owner = " (nil)";
+printf("Name: (nil)");
 
-printf("name: %s\nage: %f\nOwner: %s\n", d->name, d->age, d->owner);
+if (d->age == 0)
+printf("Age: (nil)");
+
+if (d->owner == NULL)
+printf("Owner: (nil)");
+
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
 }
