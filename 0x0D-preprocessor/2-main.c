1fbfdb3fdf60d1c03fac5fac693390ceac1910dd
joseftango <4279@holbertonschool.com>
2022-04-14 14:10:06 +0100

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
-int main()
+/**
+*2-main.c - prints the name of the file it was compiled from
+*@void: input
+*Return: Always 0
+*/
+int main(void)
 {
 
-printf("%s\n",__FILE__);
+printf("%s\n", __FILE__);
 
 return (0);
 }
