97e00bf39a38e6b58e1f725d3ef57de4de5384aa
joseftango <4279@holbertonschool.com>
2022-04-18 01:34:17 +0100

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+*int_index - function that searches for an integer
+*@array: input
+*@size: input
+*@cmp: pointer to function
+*Return: integer
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int res;
+int i;
+if (array == NULL || (cmp == NULL || size <= 0))
+return (-1);
+for (i = 0; array[i] < size; i++)
+{
+res = cmp(array[i]);
+if (res == 98 || res > 0)
+break;
+}
+return (i);
+}
