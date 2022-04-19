61f4056b47bf9be5d3c3b8e8806d89e0980babd8
joseftango <4279@holbertonschool.com>
2022-04-19 10:45:11 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,13 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+int a, b;
+a = 0;
+b = 0;
+while (s1[a] != '\0' && b == 0)
 {
-if (s1[i] == s2[i] || s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
-break;
+b = s1[a] - s2[a];
+a++;
 }
-}
-return (0);
+return (b);
 }
