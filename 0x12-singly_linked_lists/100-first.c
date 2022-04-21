b70d25aeaa859d5075dc87e109906e00226c5829
joseftango <4279@holbertonschool.com>
2022-04-21 11:44:21 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void myStartupFun(void) __attribute__ ((constructor));
+/**
+ * myStartupFun - print before main
+ * Return : 0
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
