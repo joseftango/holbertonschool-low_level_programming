39d50809ae478c41d7843179b5700aa57b5625e4
joseftango <4279@holbertonschool.com>
2022-05-16 09:36:47 +0100

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,32 +1,33 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
- * *add_node_end - adding a new node at the end
- * @head : pointer to pointer
- * @str : const char
- * Return: new nodes
- */
+*add_node_end - fuction that add new node at the end of of alist
+*@head: pointer to pointer to a struct
+*@str: pointer to string
+*Return: new node created
+**/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *ppt = *head;
-	list_t *newnode = malloc(sizeof(list_t));
+list_t *node;
+list_t *last;
 
-	if (newnode == NULL)
-		return (NULL);
-	newnode->str = strdup(str);
-	newnode->len = strlen(str);
-	newnode->next = NULL;
+node = (list_t *)malloc(sizeof(list_t));
+if (node == NULL)
+return (NULL);
+node->str = strdup(str);
+node->len = strlen(str);
+node->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = newnode;
-		return (newnode);
-	}
-	while (ppt->next != NULL)
-	{
-		ppt = ppt->next;
-	};
+if (*head == NULL)
+{
+*head = node;
+return (node);
+}
 
-	ppt->next = newnode;
-	return (newnode);
+last = *head;
+while(last->next != NULL)
+{
+last = last->next;
+}
+last->next = node;
+return(node);
 }
