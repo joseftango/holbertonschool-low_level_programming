d72f4c07f5e39a667aa11c7e6ead53cddbfe0cd7
joseftango <4279@holbertonschool.com>
2022-08-24 12:51:55 +0100

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 /**
- * sum_listint - sum of the listint_t
- * @head : pointer
- * Return: integer
- */
+* sum_listint - function that calculate the sum of all data n of a list
+* @head: pointer to a struct
+* Return: the sum of all n
+*/
 int sum_listint(listint_t *head)
 {
-	int j  = 0;
+int sum = 0;
+listint_t *h_rep;
 
-	while (head)
-	{
-	j += head->n;
-	head = head->next;
-	}
-	return (j);
+if (head == NULL)
+return (0);
+
+h_rep = head;
+while (h_rep != NULL)
+{
+sum += h_rep->n;
+h_rep = h_rep->next;
+}
+return (sum);
 }
