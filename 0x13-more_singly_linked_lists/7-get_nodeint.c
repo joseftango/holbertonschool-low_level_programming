3271c9e97f10c1197b51b3a80c624406658851ac
joseftango <4279@holbertonschool.com>
2022-08-24 12:32:08 +0100

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,13 +1,23 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - returns the nth node
- * @head : pointer
- * @index : unsigned integer
- * Return: head
- */
+* get_nodeint_at_index - function that gets thr nth node of a list
+* @head: pointer to a struct
+* @index: integer
+* Return: the nth node
+**/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	for (; head && index; index--)
-	head = head->next;
-	return (head);
+unsigned int count = 0;
+listint_t *current = head;
+if (head == NULL)
+return (NULL);
+
+while (current->next != NULL)
+{
+if (count == index)
+break;
+count++;
+current = current->next;
+}
+return (current);
 }
