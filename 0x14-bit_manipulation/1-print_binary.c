a77f9aa48bd9c623e3ab1c2a33f4bc886e78779f
joseftango <4279@holbertonschool.com>
2022-08-25 15:23:05 +0100

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * traversal - compares every 0 and 1 present in n with 1
+ * @n: integer
+ * Return: void
+ */
+void traversal(unsigned long int n)
+{
+if (n == 0)
+return;
+
+traversal(n >> 1);
+
+if ((n & 1) == 1)
+_putchar('1');
+else
+_putchar('0');
+}
+/**
+ * print_binary - prints binary from decimal
+ * @n: %ul integer
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+traversal(n);
+}
