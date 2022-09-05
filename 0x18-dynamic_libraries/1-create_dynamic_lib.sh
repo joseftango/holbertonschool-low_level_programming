d57df2e98a47ed5a9b8614708a79c435ce92501a
joseftango <4279@holbertonschool.com>
2022-09-05 14:45:51 +0100

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra *.c -c -fpic
-gcc -Wall -pedantic -Werror -Wextra *.o -shared -o liball.so
+gcc -Wall -pedantic -Werror -Wextra *.c -c -fpic | gcc -Wall -pedantic -Werror -Wextra *.o -shared -o liball.so
