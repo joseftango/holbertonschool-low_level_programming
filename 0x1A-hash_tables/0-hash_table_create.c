c4ee0143bf6af1ee657b59da8d0a703f5d93e5c0
joseftango <4279@holbertonschool.com>
2022-09-10 17:21:40 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,6 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-unsigned long int i;
 hash_table_t *ht;
 
 ht = malloc(sizeof(hash_table_t));
@@ -18,8 +17,8 @@ return (NULL);
 ht->size = size;
 ht->array = malloc(sizeof(hash_node_t *) * size);
 
-for (i = 0; i < size; i++)
-ht->array[i] = NULL;
+if (ht->array == NULL)
+return (NULL);
 
 return (ht);
 }
