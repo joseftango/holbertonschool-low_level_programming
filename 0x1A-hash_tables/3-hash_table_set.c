39133ecd18505c5ba03a629ed65fdbba61efca45
joseftango <4279@holbertonschool.com>
2022-09-15 13:22:36 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,18 +10,35 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 int index = 0;
+hash_node_t *tmp = NULL;
 hash_node_t *new = malloc(sizeof(hash_node_t));
 
-if (ht == NULL || (new == NULL || key == NULL))
+if ((ht == NULL || ht->array ==NULL) || (new == NULL || key == NULL))
 return (0);
 
 index = key_index((const unsigned char *)key, ht->size);
 
+tmp = ht->array[index];
+
+while (tmp != NULL)
+{
+if (strcmp(tmp->key, key) == 0)
+{
+free(tmp->value);
+tmp->value = strdup(value);
+return (1);
+}
+
+tmp = tmp->next;
+}
+
+
 new->key = strdup(key);
 new->value = strdup(value);
 
-if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+if (ht->array[index] != NULL)
 ht->array[index]->next = new;
+
 
 else
 ht->array[index] = new;
